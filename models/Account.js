4e5005bedff3dba2f51b21d789e2de0741a1a9a9
Luciano Miranda <lucianojmf@gmail.com>
2014-01-09 09:27:20 -0200

    first

---
+++ models/Account.js
@@ -0,0 +1,124 @@
+module.exports = function(config, mongoose, nodemailer) {
+	var crypto = require('crypto');
+
+	//Modelo de Status (post) --------------------------------------------
+	var Status = new mongoose.Schema({
+		name: {
+			first:{ type: String },
+			last:{ type: String }
+		},
+		status:{ type: String }
+	});
+	//-------------------------------------------------------------------
+
+
+	// Modelo de conta --------------------------------------------------
+	var AccountSchema = new mongoose.Schema({
+		email: { type: String, unique: true },
+		
+		password: { type: String },
+		
+		name: {
+			first: { type: String},
+			last: { type: String}
+		},
+		
+		birthday: {
+			day: {type: Number, min:1, max:31, required: false},
+			month: {type: Number, min:1, max:12, required: false},
+			year: {type: Number}
+		},
+		photoUrl: { type: String},
+		biography: { type: String},
+		status: [Status], //Updates de status do usuário
+		activity: [Status] //Todas as atualizações de status dos amigos
+	});
+	//------------------------------------------------------------------
+	var Account = mongoose.model('Account', AccountSchema);
+
+	//Retorno do registro da conta
+	var registerCallback = function(err) {
+		if (err) {
+			return console.log(err);
+		};
+		return console.log('Conta criada com sucesso!');
+	};
+
+	//Muda a senha do usuário
+	var changePassword = function(accountId, newpassword) {
+		var shaSum = crypto.createHash('sha256');
+		shaSum.update(newpassword);
+		var hashedPassword = shaSum.digest('hex');
+		
+		Account.update({_id:accountId}, {$set: {password:hashedPassword}},{upsert:false},
+			function changePasswordCallback(err) {
+				console.log('Change password done for account ' + accountId);
+		});
+	};
+
+	//Usuario perdeu a senha
+	var forgotPassword = function(email, resetPasswordUrl, callback) {
+		var user = Account.findOne({email: email}, function findAccount(err, doc){
+			if (err) {
+				// Email address is not a valid user
+				callback(false);
+			} else {
+				var smtpTransport = nodemailer.createTransport('SMTP', config.mail);
+				resetPasswordUrl += '?account=' + doc._id;
+				smtpTransport.sendMail({
+					from: 'thisapp@example.com',
+					to: doc.email,
+					subject: 'Petiko: Pedido de senha',
+					text: 'Clique aqui para modificar sua senha: ' + resetPasswordUrl
+				}, function forgotPasswordResult(err) {
+					if (err) {
+						callback(false);
+					} else {
+						callback(true);
+					}
+				});
+			}
+		});
+	};
+
+	//Atuentica o usuário
+	var login = function(email, password, callback) {
+		var shaSum = crypto.createHash('sha256');
+		shaSum.update(password);
+		Account.findOne({email:email,password:shaSum.digest('hex')},function(err,doc){
+			callback(null!=doc);
+		});
+	};
+
+	//Recupera usuario por id
+	var findById = function(accountId, callback) {
+		Account.findOne({_id:accountId}, function(err,doc) {
+			callback(doc);
+		});
+	}
+
+	//Registra um novo usuario
+	var register = function(email, password, firstName, lastName) {
+		var shaSum = crypto.createHash('sha256');
+		shaSum.update(password);
+		console.log('Registering ' + email);
+		var user = new Account({
+			email: email,
+			name: {
+				first: firstName,
+				last: lastName
+			},
+			password: shaSum.digest('hex')
+		});
+		user.save(registerCallback);
+		console.log('Save command was sent');
+	}
+	return {
+		findById: findById,
+		register: register,
+		forgotPassword: forgotPassword,
+		changePassword: changePassword,
+		login: login,
+		Account: Account
+	}
+}
