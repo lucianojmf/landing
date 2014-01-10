62865f755a72defe9b4e919058f1f975db457bb9
dexterorion <viniciusnordiesperanca@gmail.com>
2014-01-10 17:22:57 -0200

    teste

--- server/models/User.js
+++ server/models/User.js
@@ -10,12 +10,12 @@ var
     , mongoose = require('mongoose')
     , userRoles =       require('../../client/js/routingConfig').userRoles;
 
-var PetSchema = new moongoose.Schema({
-            nomePet: { type: String },
-            tipoPet: { type: String },
-            racaPet: { type: String },
-            nascimentoPet: { type: Date }
-});
+// var PetSchema = new moongoose.Schema({
+//             nomePet: { type: String },
+//             tipoPet: { type: String },
+//             racaPet: { type: String },
+//             nascimentoPet: { type: Date }
+// });
 
 // Modelo de conta --------------------------------------------------
 var UserSchema = new mongoose.Schema({
@@ -34,7 +34,7 @@ var UserSchema = new mongoose.Schema({
         last: { type: String,  index: { unique: false }}
     }
 
-    pets : [pet]
+    // pets : [pet]
 
     // birthday: {
     //     day: {type: Number, min:1, max:31, required: false},
