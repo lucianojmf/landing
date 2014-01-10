02a36f581c9809a39291d77524e37b2b9816a04d
dexterorion <viniciusnordiesperanca@gmail.com>
2014-01-10 18:35:28 -0200

    teste

--- client/js/services.js
+++ client/js/services.js
@@ -32,7 +32,7 @@ angular.module('petiko')
             }).error(error);
         },
         registerPets: function(user, pets, sucess, error){
-            alert(JSON.stringify(user));
+            alert(JSON.stringify(pets));
             $http.post('/registerPets', user, pets).success(function(res){
                 success(user);
             }).error(error);
